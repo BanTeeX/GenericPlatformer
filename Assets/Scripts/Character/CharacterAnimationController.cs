9ade9f2e0c1a9881a7321f246702d0615c5b0eaf
Filip <filipbanaszak333@gmail.com>
2020-10-30 15:34:44 +0100

    Add prefabs, character movement and animations, PlayerDestroyer script

---
+++ Assets/Scripts/Character/CharacterAnimationController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+	private Animator animator;
+	private Rigidbody2D _rigidbody;
+	private CharacterMovement controller;
+	private bool isJumping;
+	private bool isFalling;
+	private bool isMoving;
+	private bool isFliped;
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+		_rigidbody = GetComponent<Rigidbody2D>();
+		controller = GetComponent<CharacterMovement>();
+	}
+
+	private void Update()
+	{
+		animator.SetBool("isJumping", isJumping);
+		animator.SetBool("isFalling", isFalling);
+		animator.SetBool("isMoving", isMoving);
+		animator.SetBool("isGrounded", controller.IsGrounded);
+		GetComponent<SpriteRenderer>().flipX = isFliped;
+	}
+
+	private void FixedUpdate()
+	{
+		if (_rigidbody.velocity.y > 0)
+		{
+			isJumping = true;
+		}
+		else
+		{
+			isJumping = false;
+		}
+		if (_rigidbody.velocity.y < 0)
+		{
+			isFalling = true;
+		}
+		else
+		{
+			isFalling = false;
+		}
+		if (_rigidbody.velocity.x != 0)
+		{
+			isMoving = true;
+			if (_rigidbody.velocity.x < 0)
+			{
+				isFliped = true;
+			}
+			else
+			{
+				isFliped = false;
+			}
+		}
+		else
+		{
+			isMoving = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+	public bool IsGrounded { get; private set; }
+
+	[SerializeField] private float jumpForce = 500f;
+	[SerializeField] private float moveSpeed = 5f;
+	[SerializeField] private Transform chceckGround;
+	[SerializeField] private float checkGroundRadius = 0.1f;
+	[SerializeField] private LayerMask whatIsGround;
+	[SerializeField] private bool airControl = true;
+	//[SerializeField] private float moveSmooth = 0.3f; //manualSmooth
+
+	//private Vector2 velocity = Vector2.zero; //manualSmooth
+	private Rigidbody2D _rigidbody;
+	private float horizontalVelocity = 0f;
+	private bool jump = false;
+
+	private void Start()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		//horizontalVelocity = Input.GetAxisRaw("Horizontal") * moveSpeed; //manualSmooth
+		horizontalVelocity = Input.GetAxis("Horizontal") * moveSpeed; //autoSmooth
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		IsGrounded = Physics2D.OverlapCircle(chceckGround.position, checkGroundRadius, whatIsGround);
+		if (IsGrounded || airControl)
+		{
+			//Vector2 targetVelocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y); //manualSmooth
+			//_rigidbody.velocity = Vector2.SmoothDamp(_rigidbody.velocity, targetVelocity, ref velocity, moveSmooth); //manualSmooth
+			_rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y); //autoSmooth
+		}
+		if (jump && IsGrounded)
+		{
+			_rigidbody.AddForce(new Vector2(0, jumpForce));
+			jump = false;
+		}
+	}
+}
