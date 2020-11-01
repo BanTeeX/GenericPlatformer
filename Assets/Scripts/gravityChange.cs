38cde408e4bb4e114188c1487aa1bda9048fcc15
Patryk <patrykmrtv@gmail.com>
2020-11-01 21:32:42 +0100

    Gravity script

---
+++ Assets/Scripts/gravityChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityChange : MonoBehaviour
+{
+
+	
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().gravityScale *= -1; ;
+			GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().rotation += 180;
+			FindObjectOfType<CharacterMovement>().jumpForce *= -1;
+		}
+	}
+
+	
+}
